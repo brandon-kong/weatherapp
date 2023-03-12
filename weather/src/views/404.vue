4017ee33db8182f88d6902c55bbf2a8ca67107e7
konginator <69498235+konginator@users.noreply.github.com>
2023-03-11 23:11:55 -0600

    404 padding

--- weather/src/views/404.vue
+++ weather/src/views/404.vue
@@ -1,9 +1,24 @@
 <template>
-    <div>
+    <div class="error-container">
         bruh what are u doing
     </div>
 </template>
 
+<style scoped>
+
+ .error-container {
+    display: flex;
+    min-height: 100vh;
+    width: 100%;
+    height: fit-content;
+    box-sizing: border-box;
+    padding: var(--outer-padding);
+
+    text-align: center;
+    grid-column-gap: calc(var(--outer-padding) * 2);
+    flex-grow: 1;
+}
+</style>
 <script>
 
 export default {
